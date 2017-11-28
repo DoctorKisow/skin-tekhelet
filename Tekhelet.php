542948cc3c6333227332f7fc9636f018d22ba5d7
Dr. Kisow <matt.kisow@gmail.com>
2017-11-28 09:58:01 -0500

    Copyright Update.

--- Tekhelet.php
+++ Tekhelet.php
@@ -2,7 +2,7 @@
 /**
   * Tekhelet - The look of the Kisow Foundation Wiki.
   * Matthew R. Kisow, D.Sc. <matthew.kisow@kisow.org>
-  * Copyright 2015-2017, Kisow Foundation, Inc.
+  * Copyright, Kisow Foundation, Inc. 2015-2017.
   *
   * The Tekhelet theme is based in part on the Tyrian theme by Alex Legler.
  **/
